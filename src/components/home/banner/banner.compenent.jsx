5d49d07e6504156b715abcdbcfc67f32bf92991a
graceDev1 <grace.devolop@gmail.com>
2021-03-09 12:47:10 +0200

    fixed banner

--- src/components/home/banner/banner.compenent.jsx
+++ src/components/home/banner/banner.compenent.jsx
@@ -11,8 +11,8 @@ function Banner() {
         <div id="carouselExampleSlidesOnly" className="carousel slide carousel-fade" data-bs-ride="carousel">
         <div class="carousel-inner">
          {/* <BannerItem image={poule}/> */}
-         {/* <BannerItem image={agro}/> */}
-         <BannerItem image={vegetable}/>
+         <BannerItem image={agro}/>
+         {/* <BannerItem image={vegetable}/> */}
         </div>
       </div>
     )
