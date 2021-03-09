0f9e93bebc2319a3ba8eb444cb4b0a0831920c58
graceDev1 <grace.devolop@gmail.com>
2021-03-09 15:08:54 +0200

    partners

--- src/components/partnes/partner.component.jsx
+++ src/components/partnes/partner.component.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import PartnerItem from './partitem/partneritem.component';
+import './partner.component.css';
+import somban from '../../assets/sombabien.png'
 
 function Partner() {
     return (
-        <div>
-            
+        <div style={{
+            marginTop:'150px',
+            marginBottom:'150px'
+        }} className="container">
+            <h1>Our Partners</h1>
+            <div className="column">
+                <PartnerItem
+                img={somban}
+                />
+                <PartnerItem
+                img={somban}
+                />
+                <PartnerItem
+                img={somban}
+                />
+            </div>
         </div>
     )
 }
