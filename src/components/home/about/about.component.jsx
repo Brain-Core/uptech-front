8b33bcdd3fb9ee36f64ececbd0bc265597019e5e
bvenceslas <bvenceslas@gmail.com>
2021-03-17 16:15:19 +0200

    update the about text

--- src/components/home/about/about.component.jsx
+++ src/components/home/about/about.component.jsx
@@ -6,32 +6,17 @@ function About() {
             <div className="col">
                 <div className="col-md-22 mt-4">
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste voluptatum animi nostrum odio 
-                        illo inventore aliquam. Doloremque nisi porro ipsam eveniet 
-                        necessitatibus dicta. Magni, consequuntur perferendis non consectetur deserunt asperiores.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, a
-                        ccusamus porro esse asperiores harum accusantium eum? Nulla rem ea, 
-                        alias deserunt aut similique nemo ducimus tempora, autem ab in culpa.
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste voluptatum animi nostrum odio 
-                        illo inventore aliquam. Doloremque nisi porro ipsam eveniet 
-                        necessitatibus dicta. Magni, consequuntur perferendis non consectetur deserunt asperiores.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, a
-                        ccusamus porro esse asperiores harum accusantium eum? Nulla rem ea, 
-                        alias deserunt aut similique nemo ducimus tempora, autem ab in culpa.
+                    Umoja Poultry farm technologies is an innovative agricultural technology services company
+                    that strives to increase the access and use of quality and reasonably priced poultry inputs to
+                    young entrepreneurs working in poultry farming in the North-kivu province in general
+                    and in the city of Goma in particular. 
                     </p>
                     <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste voluptatum animi nostrum odio 
-                        illo inventore aliquam. Doloremque nisi porro ipsam eveniet 
-                        necessitatibus dicta. Magni, consequuntur perferendis non consectetur deserunt asperiores.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, a
-                        ccusamus porro esse asperiores harum accusantium eum? Nulla rem ea, 
-                        alias deserunt aut similique nemo ducimus tempora, autem ab in culpa.
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste voluptatum animi nostrum odio 
-                        illo inventore aliquam. Doloremque nisi porro ipsam eveniet 
-                        necessitatibus dicta. Magni, consequuntur perferendis non consectetur deserunt asperiores.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, a
-                        ccusamus porro esse asperiores harum accusantium eum? Nulla rem ea, 
-                        alias deserunt aut similique nemo ducimus tempora, autem ab in culpa.
+                    The team is developing technologies that allow smallholder farmers to access affordable, 
+                    quality inputs and other agricultural services in a timely manner. 
+                    The UP-Tech initiative aims to improve the productivity of small farmers, 
+                    increase their profit margins to more than 20%, ensure food self-sufficiency 
+                    in white meat in the region and create value for its customers and consumers.
                     </p>
                 </div>
             </div>
