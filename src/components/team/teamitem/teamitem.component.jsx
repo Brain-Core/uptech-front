7ee8925961333f645e1a1273ccaceb163183066c
bvenceslas <bvenceslas@gmail.com>
2021-06-10 06:25:11 +0200

    align team part

--- src/components/team/teamitem/teamitem.component.jsx
+++ src/components/team/teamitem/teamitem.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function TeamItem({img,name,position}) {
     return (
-        <div className="col-xs-6 col-md-4 mt-4 text-center">
+        <div className="col-xs-6 col-md-4 mt-4 text-center" style={{margin: '0 auto'}}>
             <img
             style={{
                 width:'50%'
@@ -10,7 +10,7 @@ function TeamItem({img,name,position}) {
             className="img-fluid"
              src={img} 
              alt="img_name"/>
-            <p className="font-weight-bold mt-4">{name}</p>
+            <p className="font-weight-bold mt-2">{name}</p>
             <p className="small mt-0">{position}</p>
         </div>
     )
