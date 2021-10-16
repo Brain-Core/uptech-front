2c188a7f36094f831aaa6f4d32ee7c7d8cb8fe25
bvenceslas <bvenceslas@gmail.com>
2021-10-16 12:56:52 +0200

    update email addresss

--- src/components/header/header_top/headertop.component.jsx
+++ src/components/header/header_top/headertop.component.jsx
@@ -16,7 +16,7 @@ function HeaderTop() {
                  src={logo}
                   alt="..."/>
                 </Link>
-                <p className="header__mail">info@up-tech.com</p>
+                <p className="header__mail">info@uptech-agro.com</p>
                 <p className="header__phone">+243 823 751 631</p>
             </div>
             <div>
